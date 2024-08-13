ae7e14807dc076d278d8974eca81cb4825064de9
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-08-13 19:55:35 -0300

    update H2h.css, H2h.tsx, LineUp.css and Matches.tsx

--- src/components/H2h/H2h.tsx
+++ src/components/H2h/H2h.tsx
@@ -1,5 +1,5 @@
 import './H2h.css'
-import { ApiH2h } from '../../interfaces/H2h'
+import { ApiH2h } from '../../interfaces/h2h'
 
 interface H2hProps {
     h2h: ApiH2h[]
@@ -13,6 +13,8 @@ const H2h: React.FC<H2hProps> = ({ h2h }) => {
 
     return (
         <div className='h2h'>
+            <h3 className='box__title'>Ultimos Confrontos</h3>
+            {!lastH2h || lastH2h.length === 0 && <h5>Não possui jogos anteriores.</h5>}
             {lastH2h.map((element, index) => {
                 if (index > 4) return
                 return (
@@ -20,12 +22,12 @@ const H2h: React.FC<H2hProps> = ({ h2h }) => {
                         <span className='item__date' >{new Date(element.startTimestamp * 1000).toLocaleString().slice(0, 10)}</span>
                         <span className='item__date'>{element.tournament.name} </span>
                         <div className="item__data">
-                            <span className='item__data-name'>{element.homeTeam.shortName}</span>
+                            <span className='item__data-name'><img src={`https://api.sofascore.app/api/v1/team/${element.homeTeam.id}/image`} alt={element.homeTeam.shortName} />{element.homeTeam.shortName}</span>
                             <h5 className='item__data-score'>{element.homeScore.current}</h5>
                         </div>
                         X
                         <div className="item__data reverse">
-                            <span className='item__data-name'>{element.awayTeam.shortName}</span>
+                            <span className='item__data-name'>{element.awayTeam.shortName} <img src={`https://api.sofascore.app/api/v1/team/${element.awayTeam.id}/image`} alt={element.awayTeam.shortName} /> </span>
                             <h5 className='item__data-score'>{element.awayScore.current}</h5>
                         </div>
                     </div>
